942f7b752e83b9c72b8d5c304fadce386c0a197a
ARGoncharov <gonlex@mail.ru>
2023-09-27 15:56:13 +0300

    Adding SearchField

---
+++ src/components/SearchComponent.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+  .search-field-component
+    input.search-field(type="search" placeholder="Искать гифку..." required)
+</template>
+
+<script>
+export default {
+  name: 'SearchComponent',
+  methods: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.search-field-component {
+  display: flex;
+  justify-content: center;
+
+  .search-field {
+    appearance: none;
+    border: none;
+    outline: none;
+    background-color: gainsboro;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.15);
+    display: block;
+    width: 100%;
+    max-width: 600px;
+    padding: 15px;
+    border-radius: 10px;
+    font-size: 20px;
+    &::placeholder {
+      color: black;
+    }
+    &:focus,
+    &:valid
+    {
+      background-color: black;
+      color: gainsboro;
+    }
+  }
+}
+</style>
